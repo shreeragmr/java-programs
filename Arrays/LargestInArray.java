1a78ad0ebbef4ce71f75200681a8503cf927e96c
shreeragmr <shreeragmr@gmail.com>
2026-03-07 23:26:13 +0530

    LargestInArray

---
+++ Arrays/LargestInArray.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class LargestInArray {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {10,45,23,89,67};
+
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest number: "+max);
+    }
+
+}
